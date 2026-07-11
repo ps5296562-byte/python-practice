0dbb15fed0f5debdedc49c939cd45b04dce3d81f
Poonam Soni <ps5296562@gmail.com>
2026-07-11 18:45:02 +0530

    Add mad libs generator project

---
+++ mad_libs_generator.py
@@ -0,0 +1,5 @@
+noun = input("Enter a noun:")
+verb =  input("Enter a verb:")
+adjective =  input("Enter an adjective:")
+place = input("Enter a place:")
+print(f"The {adjective} {noun} {verb} the ancient temple in  {place}")
