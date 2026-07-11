802ed49eb2f228ed60d78fa95b6954bbc54b3c5b
Poonam Soni <ps5296562@gmail.com>
2026-07-11 20:39:32 +0530

    Add quiz game project

---
+++ quiz_game.py
@@ -0,0 +1,21 @@
+print("Welcome to the Quiz Game!")
+question = ("\n1. Who is the Prime Minister of India?")
+print("A. Mr. Narendra Modi")
+print("B. Mamta Banerjee")
+print("C. Rahul Gandhi")
+print("D. Vashundhra Raje")
+answer = input("Your answer is (A/B/C/D):").upper()
+if answer == "A":
+    print("Correct")
+else:
+    print("Incorrect")
+question = ("\n2. Which of the following is the capital of Rajasthan?")
+print("A. Delhi")
+print("B. Agartala")
+print("C. Dausa")
+print("D. Jaipur")
+answer = input("Your answer is (A/B/C/D):").upper()
+if answer == "D":
+    print("Correct")
+else:
+    print("Incorrect")
